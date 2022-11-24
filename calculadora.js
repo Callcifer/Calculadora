838f59d958949c1276ab62b7850d6a791e77257e
callcifer <c.andresrojas1995@gmail.com>
2022-11-24 16:11:18 -0500

    la calculadora inicio

---
+++ calculadora.js
@@ -0,0 +1,20 @@
+var x , y , z 
+function esteesminumero(Numero) {
+if (z==undefined) {
+    x+=Numero
+}
+if (y==undefined) {
+    y=Numero
+    
+}
+}
+function igual(){
+    mih2 =document.getElementById("resultado")
+    mih2.innerText=z
+}
+function suma() {
+    z = x + y
+}
+function resta() {
+    z = x - y
+}
